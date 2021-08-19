9e5ed31db8870fb145942afa15393c6d75293c04
Christiaan0317 <66475599+Christiaan0317@users.noreply.github.com>
2021-08-19 13:49:33 +0200

    Initial commit

---
+++ repoTest.py
@@ -0,0 +1,6 @@
+num1 = int(input("please insert one number here of your choice: "))
+num2 = int(input("please insert a number you wish to raise to the power of :"))
+float1 = num1
+for x in range(num2):
+    float1 = float1* num1
+print(float1)
